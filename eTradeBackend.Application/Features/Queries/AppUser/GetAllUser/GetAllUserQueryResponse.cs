fcb91af16602d201658219668a1442490e7d161b
Eshat ÇELİK <eshat.celik@procenne.com>
2023-09-14 01:10:13 +0300

    writing query and commands

---
+++ eTradeBackend.Application/Features/Queries/AppUser/GetAllUser/GetAllUserQueryResponse.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eTradeBackend.Application.Features.Queries.AppUser.GetAllUser
+{
+    public class GetAllUserQueryResponse
+    {
+        public object Users { get; set; }
+        public int TotalUserCount { get; set; }
+    }
+}
---
+++ eTradeBackend.Application/Services/IFileService.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eTradeBackend.Application.Services
+{
+    public interface IFileService
+    {
+        Task<List<(string fileName, string path)>> UploadAsync(string path, IFormFileCollection formFiles);
+        Task<bool> CopyFileAsync(string path, IFormFile file);
+    }
+}
