fdca2b208aa024255c1d064607fea04a238f9ca6
ISSISTEM\eshat.celik <eshat.celik@issistem.com>
2023-10-06 01:35:00 +0300

    added persistance lib an contextDb class created

---
+++ eTradeBackend.Persistance/Contexts/eTradeDbContext.cs
@@ -0,0 +1,14 @@
+﻿using eTradeBackend.Domain.Entities.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eTradeBackend.Persistance.Contexts
+{
+    public class eTradeDbContext:IdentityDbContext<AppUser>
+    {
+    }
+}
